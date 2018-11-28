3992397d649c3e17efa1bea4f421bcf90fad1c6d
mgrabuszynski <mgrabuszynski@gmail.com>
2018-11-28 03:38:27 +0100

    Better .gitignore

---
+++ main.py
@@ -0,0 +1,22 @@
+# from pprint import PrettyPrinter
+#
+# from server import serializer
+# from server import graph_checker
+
+from . import server
+
+if __name__ == "__main__":
+    server.run_server()
+
+    # pp = PrettyPrinter()
+    # with open('input_sample.txt') as file:
+    #     message = file.read()
+    #
+    # extracted = serializer.extract_string(message)
+    # print(extracted)
+    #
+    # series = serializer.text_to_series(extracted)
+    # print(series)
+    #
+    # checked = graph_checker.check_words(extracted, series)
+    # pp.pprint(checked)
